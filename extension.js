6970804e5175091933c7a4d9cdd654371e5be939
Kurt Moser <kurt@suvaline.com>
2019-07-22 20:58:44 +0300

    Add menuitem to open current branch url

--- extension.js
+++ extension.js
@@ -12,14 +12,17 @@ function activate(context) {
         {
             domain: 'github.com',
             url: '{protocol}://{domain}/{base}/{name}/tree/master/{path}#L{line}',
+            urlCurrentBranch: '{protocol}://{domain}/{base}/{name}/tree/{branch}/{path}#L{line}',
         },
         {
             domain: 'bitbucket.org',
             url: '{protocol}://{domain}/{base}/{name}/src/master/{path}#lines-{line}',
+            urlCurrentBranch: '{protocol}://{domain}/{base}/{name}/src/{branch}/{path}#lines-{line}',
         },
         {
             domain: 'gitlab.com',
             url: '{protocol}://{domain}/{base}/{name}/blob/master/{path}#L{line}',
+            urlCurrentBranch: '{protocol}://{domain}/{base}/{name}/blob/{branch}/{path}#L{line}',
         },
     ];
 
@@ -49,6 +52,14 @@ function activate(context) {
         return res;
     }
 
+    async function getGitCurrentBranch(dirname) {
+        const { stdout, stderr } = await exec('git rev-parse --abbrev-ref HEAD', { cwd: dirname });
+
+        let res = stdout.replace(/\r?\n|\r/g, '');
+
+        return res;
+    }
+
     async function giturlOpenWrapper() {
         let fileName = vscode.window.activeTextEditor.document.fileName;
         let dirName = path.dirname(fileName);
@@ -72,12 +83,52 @@ function activate(context) {
         repoConf.forEach(conf => {
             if (repoHost.match(conf.domain)) {
                 url = conf.url;
-                url = url.replace('{host}', repoHost);
-                url = url.replace('{schema}', repoSchema);
+                url = url.replace('{domain}', repoHost);
+                url = url.replace('{protocol}', repoSchema);
                 url = url.replace('{base}', repoBase);
                 url = url.replace('{name}', repoName);
                 url = url.replace('{path}', relativePath);
                 url = url.replace('{line}', activeLine);
+            }
+        });
+
+        if (url) {
+            vscode.env.openExternal(vscode.Uri.parse(url));
+        }
+    }
+
+    async function giturlOpenCurrentBranchWrapper() {
+        let fileName = vscode.window.activeTextEditor.document.fileName;
+        let dirName = path.dirname(fileName);
+
+        let localBase = await getGitLocalBase(dirName);
+        let currentBranch = await getGitCurrentBranch(dirName);
+
+        res = await getGitConfig(dirName);
+
+        let activeLine = vscode.window.activeTextEditor.selection.active.line + 1;
+
+        const urlHandler = require('url');
+        const pathHandler = require('path');
+        let repoSchema = 'https';
+        let repoUrlParts = urlHandler.parse(res.remoteRepoUrl);
+        let relativePath = fileName.substring(localBase.length + 1);
+        let repoHost = repoUrlParts.hostname;
+        let repoBase = pathHandler.basename(pathHandler.dirname(repoUrlParts.path));
+        let repoName = pathHandler.basename(repoUrlParts.path);
+
+        let url = null;
+        repoConf.forEach(conf => {
+            if (repoHost.match(conf.domain)) {
+                console.log(conf);
+                url = conf.urlCurrentBranch;
+                url = url.replace('{domain}', repoHost);
+                url = url.replace('{protocol}', repoSchema);
+                url = url.replace('{base}', repoBase);
+                url = url.replace('{name}', repoName);
+                url = url.replace('{path}', relativePath);
+                url = url.replace('{line}', activeLine);
+                url = url.replace('{branch}', currentBranch);
             }
         });
 
@@ -94,13 +145,23 @@ function activate(context) {
         }
     }
 
-    let disposable = vscode.commands.registerCommand('giturl.open', function () {
+    let disposables = [];
+
+    disposables.push(vscode.commands.registerCommand('giturl.open', function () {
         readUserConfig();
 
         giturlOpenWrapper();
-    });
+    }));
 
-    context.subscriptions.push(disposable);
+    disposables.push(vscode.commands.registerCommand('giturl.open-currentbranch', function () {
+        readUserConfig();
+
+        giturlOpenCurrentBranchWrapper();
+    }));
+
+    console.log(disposables);
+
+    context.subscriptions.push(disposables);
 }
 exports.activate = activate;
 
